3bf547864392f5e8e5251efdbc3150b8357e07d3
kompasim <kompasim@163.com>
2022-08-10 20:10:40 +0800

    update

--- build.lua
+++ build.lua
@@ -1,34 +1,14 @@
 
--- pcall(os.execute, "git clone git@github.com:kompasim/my-build-tools.git ./.my-build-tools")
-package.path = package.path .. ";./.my-build-tools/?.lua"
+-- pcall(os.execute, "git clone git@github.com:kompasim/pure-js-tools.git ./pure-js-tools")
+-- pcall(os.execute, "git clone git@github.com:kompasim/my-build-tools.git ./my-build-tools")
+package.path = package.path .. ";./my-build-tools/?.lua"
 
-local HBuilder = require("html_builder")
-local CBuilder = require("c_builder")
-local CodeBuilder = require("code_builder")
+local Builder = require("html_builder")
 
-local hBuilder = HBuilder(false)
-hBuilder:inputFile("./uyghurche.html")
-hBuilder:containScript()
-hBuilder:containStyle()
-hBuilder:containImage()
-hBuilder:setOutput("./target.html")
-hBuilder:start()
-
-
-local codeBuilder = CodeBuilder(false)
-codeBuilder:setInput("./uyghurche.c")
-codeBuilder:handleMacro("//")
-codeBuilder:setOutput("./target.c")
-codeBuilder:start()
-
-local cBuilder = CBuilder(false)
-cBuilder:setInput('./target.c')
-cBuilder:setLibs("webview")
-cBuilder:setIcon('./uyghurche.ico')
-cBuilder:setOutput('uyghurche')
-cBuilder:start(false)
-
-files.delete('./target.html')
-files.delete('./target.c')
-os.execute("start ./uyghurche.exe")
-
+local builder = Builder(false)
+builder:inputFile("./origin.html")
+builder:containScript()
+builder:containStyle()
+builder:containImage()
+builder:setOutput("./uyghurche.html")
+builder:start()
